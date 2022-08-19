0ba17073bfdaa55530581187d1010de16b6b1f80
Dilmurod Djuraev <dilmurod.djuraev86@gmail.com>
2022-08-18 21:15:42 -0700

    Practice 2 file is created. branching

---
+++ src/practice2_branching.java
@@ -0,0 +1,7 @@
+public class practice2_branching {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("how is everything");
+    }
+}
